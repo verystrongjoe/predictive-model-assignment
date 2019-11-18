8215896c3a75796ef22959feab09e4791c70a18c
verystrongjoe <verystrongjoe@gmail.com>
2019-11-18 15:19:55 +0900

    add hw4

--- svm.py
+++ svm.py
@@ -1,68 +1,163 @@
-#Import scikit-learn dataset library
 from sklearn import datasets
-
-#Load dataset
-cancer = datasets.load_breast_cancer()
-
-#Import scikit-learn dataset library
-from sklearn import datasets
-
-#Load dataset
-cancer = datasets.load_breast_cancer()
-
-# Features:  ['mean radius' 'mean texture' 'mean perimeter' 'mean area'
-#  'mean smoothness' 'mean compactness' 'mean concavity'
-#  'mean concave points' 'mean symmetry' 'mean fractal dimension'
-#  'radius error' 'texture error' 'perimeter error' 'area error'
-#  'smoothness error' 'compactness error' 'concavity error'
-#  'concave points error' 'symmetry error' 'fractal dimension error'
-#  'worst radius' 'worst texture' 'worst perimeter' 'worst area'
-#  'worst smoothness' 'worst compactness' 'worst concavity'
-#  'worst concave points' 'worst symmetry' 'worst fractal dimension']
-# Labels:  ['malignant' 'benign']
-
-# print data(feature)shape
-cancer.data.shape
-
-# print the cancer data features (top 5 records)
-print(cancer.data[0:5])
-
-print(cancer.target)
-
-# Import train_test_split function
 from sklearn.model_selection import train_test_split
-
-# Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.3,random_state=109) # 70% training and 30% test
-
-
-#Import svm model
 from sklearn import svm
-
-#Create a svm Classifier
-clf = svm.SVC(kernel='linear') # Linear Kernel
-
-#Train the model using the training sets
-clf.fit(X_train, y_train)
-
-#Predict the response for test dataset
-y_pred = clf.predict(X_test)
-
-
 #Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
+from sklearn.model_selection import GridSearchCV, cross_validate
+import math
+import numpy as np
 
-# Model Accuracy: how often is the classifier correct?
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+def evaluate_for_classfication(y_test, y_pred):
+    # for classification
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import f1_score
+    from sklearn.metrics import recall_score
+    from sklearn.metrics import precision_score
+    from sklearn.metrics import roc_auc_score
+
+    print("Accuracy: ", accuracy_score(y_test, y_pred))
+    print("f1_score: ", f1_score(y_test, y_pred))
+    print("recall_score: ", recall_score(y_test, y_pred))
+    print("precision_score: ", precision_score(y_test, y_pred))
+    print("roc_auc_score: ", roc_auc_score(y_test, y_pred))
+
+
+def evaluate_for_regression(y_test, y_pred):
+    # for regression
+    from sklearn.metrics import mean_absolute_error
+    from sklearn.metrics import mean_squared_error
+    from sklearn.metrics import mean_squared_log_error
+    from sklearn.metrics import r2_score
+
+    print("mean_absolute_error: ", mean_absolute_error(y_test, y_pred))
+    print("mean_squared_error : ", mean_squared_error(y_test, y_pred))
+    print("mean_squared_log_error: ", mean_squared_log_error(y_test, y_pred))
+    print("r2_score: ", r2_score(y_test, y_pred))
+
+cancer = datasets.load_breast_cancer()
+
+print(cancer.data.shape)
+print(cancer.data[0:5])
+print(cancer.target)
+
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.3,random_state=109) # 70% training and 30% test
 
 
 """
 Tuning Hyperparameters
-
 Kernel: The main function of the kernel is to transform the given dataset input data into the required form. There are various types of functions such as linear, polynomial, and radial basis function (RBF). Polynomial and RBF are useful for non-linear hyperplane. Polynomial and RBF kernels compute the separation line in the higher dimension. In some of the applications, it is suggested to use a more complex kernel to separate the classes that are curved or nonlinear. This transformation can lead to more accurate classifiers.
-
 Regularization: Regularization parameter in python's Scikit-learn C parameter used to maintain regularization. Here C is the penalty 
 parameter, which represents misclassification or error term. The misclassification or error term tells the SVM optimization how much error is bearable. This is how you can control the trade-off between decision boundary and misclassification term. A smaller value of C creates a small-margin hyperplane and a larger value of C creates a larger-margin hyperplane.
-
 Gamma: A lower value of Gamma will loosely fit the training dataset, whereas a higher value of gamma will exactly fit the training dataset, which causes over-fitting. In other words, you can say a low value of gamma considers only nearby points in calculating the separation line, while the a value of gamma considers all the data points in the calculation of the separation line.
 """
+def svm_model(X_train, X_test, y_train, y_test, kernel):
+    print('------------------------*****{}******----------------------'.format(kernel))
+    if kernel == 'linear':
+        gsc = GridSearchCV(
+            estimator=svm.SVC(kernel=kernel),
+            param_grid={
+                'C': [0.1, 1, 100, 1000]},
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+
+        grid_result = gsc.fit(X_train, y_train)
+        best_params = grid_result.best_params_
+
+        # Create best svm Classifier
+        best_svm = svm.SVC(kernel=kernel, C=best_params["C"])
+
+        print('{} best params : {}'.format(kernel, best_params))
+
+        # Train the model using the training sets
+        best_svm.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svm.predict(X_test)
+
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_classfication(y_test, y_pred)
+
+    elif kernel == 'poly':
+
+        print('test1')
+        gsc = GridSearchCV(
+            estimator=svm.SVC(kernel=kernel),
+            param_grid={
+                'C': [100],
+                'gamma': [1, 5]},
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+        print('test2')
+
+        # gsc = GridSearchCV(
+        #     estimator=svm.SVC(kernel=kernel),
+        #     param_grid={
+        #         'C': [0.1, 1, 100],
+        #         'gamma': [0.0001, 0.005, 0.1, 1, 5]},
+        #     cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+        print('test3')
+
+        grid_result = gsc.fit(X_train, y_train)
+        print('test4')
+        best_params = grid_result.best_params_
+        print('{} best params : {}'.format(kernel, best_params))
+
+        best_svm = svm.SVC(kernel=kernel, C=best_params["C"], gamma=best_params["gamma"], coef0=best_params["coef0"])
+
+        # Train the model using the training sets
+        best_svm.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svm.predict(X_test)
+
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_classfication(y_test, y_pred)
+
+    elif kernel == 'rbf':
+        gsc = GridSearchCV(
+            estimator=svm.SVC(kernel=kernel),
+            param_grid={
+                'C': [0.1, 1, 100, 1000],
+                'gamma': [0.0001, 0.005, 0.1, 1, 5]},
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+
+        grid_result = gsc.fit(X_train, y_train)
+        best_params = grid_result.best_params_
+        print('{} best params : {}'.format(kernel, best_params))
+
+        best_svm = svm.SVC(kernel=kernel, C=best_params["C"], gamma=best_params["gamma"])
+
+        # Train the model using the training sets
+        best_svm.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svm.predict(X_test)
+
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_classfication(y_test, y_pred)
+
+
+    elif kernel == 'sigmoid':
+        gsc = GridSearchCV(
+            estimator=svm.SVC(kernel=kernel),
+            param_grid={
+                'C': [0.1, 1, 100, 1000],
+                'gamma': [0.0001, 0.005, 0.1, 1, 5],
+                'coef0': [0.1, 0.01, 0.0001]},
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+
+        grid_result = gsc.fit(X_train, y_train)
+        best_params = grid_result.best_params_
+        print('{} best params : {}'.format(kernel, best_params))
+        best_svm = svm.SVC(kernel=kernel, C=best_params["C"], gamma=best_params["gamma"], coef0=best_params["coef0"])
+
+        # Train the model using the training sets
+        best_svm.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svm.predict(X_test)
+
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_classfication(y_test, y_pred)
+
+
+
+svm_model(X_train, X_test, y_train, y_test, 'linear')
+# svm_model(X_train, X_test, y_train, y_test, 'poly')
+svm_model(X_train, X_test, y_train, y_test, 'rbf')
+svm_model(X_train, X_test, y_train, y_test, 'sigmoid')
--- svr3.py
+++ svr3.py
@@ -5,101 +5,176 @@ import math
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import GridSearchCV, cross_validate
 from sklearn.utils import shuffle
 from sklearn.datasets import load_boston
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
 
-boston_dataset = load_boston()
-boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
-boston['MEDV'] = boston_dataset.target
-boston.head()
+def evaluate_for_regression(y_test, y_pred):
+    # for regression
+    from sklearn.metrics import mean_absolute_error
+    from sklearn.metrics import mean_squared_error
+    from sklearn.metrics import mean_squared_log_error
+    from sklearn.metrics import r2_score
 
-X = boston.iloc[:, [0, 12]]
-y = boston.iloc[:, 13]
+    print("mean_absolute_error: ", mean_absolute_error(y_test, y_pred))
+    print("mean_squared_error : ", mean_squared_error(y_test, y_pred))
+    print("mean_squared_log_error: ", mean_squared_log_error(y_test, y_pred))
+    print("r2_score: ", r2_score(y_test, y_pred))
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-X = scaler.fit_transform(X)
+def svr_model(X_train, X_test, y_train, y_test, kernel):
 
-def svr_model(X, y):
-    gsc = GridSearchCV(
-        estimator=SVR(kernel='linear'),
-        param_grid={
-            'C': [0.1, 1, 100, 1000],
-            'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
-        },
-        cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+    if kernel == 'linear':
+        gsc = GridSearchCV(
+            estimator=SVR(kernel='linear'),
+            param_grid={
+                'C': [0.1, 1, 100, 1000],
+                'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
+            },
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
 
-    grid_result = gsc.fit(X, y)
-    best_params = grid_result.best_params_
-    best_svr = SVR(kernel='linear', C=best_params["C"], epsilon=best_params["epsilon"], coef0=0.1, shrinking=True,
-                   tol=0.001, cache_size=200, verbose=False, max_iter=-1)
+        grid_result = gsc.fit(X_train, y_train)
+        best_params = grid_result.best_params_
 
-    scoring = {
-               'abs_error': 'neg_mean_absolute_error',
-               'squared_error': 'neg_mean_squared_error'}
+        print('{} best params : {}'.format('linear', best_params))
+        best_svr = SVR(kernel='linear', C=best_params["C"], epsilon=best_params["epsilon"], coef0=0.1, shrinking=True,
+                       tol=0.001, cache_size=200, verbose=False, max_iter=-1)
 
-    scores = cross_validate(best_svr, X, y, cv=10, scoring=scoring, return_train_score=True)
-    return "MAE :", abs(scores['test_abs_error'].mean()), "| RMSE :", math.sqrt(abs(scores['test_squared_error'].mean()))
+        # Train the model using the training sets
+        best_svr.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svr.predict(X_test)
 
-
-def svr_model(X, y):
-    gsc = GridSearchCV(
-        estimator=SVR(kernel='poly'),
-        param_grid={
-            'C': [0.1, 1, 100, 1000],
-            'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
-            'degree': [2, 3, 4],
-            'coef0': [0.1, 0.01, 0.001, 0.0001]},
-        cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
-
-    grid_result = gsc.fit(X, y)
-    best_params = grid_result.best_params_
-    best_svr = SVR(kernel='poly', C=best_params["C"], epsilon=best_params["epsilon"], coef0=best_params["coef0"],                   degree=best_params["degree"], shrinking=True,
-                   tol=0.001, cache_size=200, verbose=False, max_iter=-1)
-
-    scoring = {
-               'abs_error': 'neg_mean_absolute_error',
-               'squared_error': 'neg_mean_squared_error'}
-
-    scores = cross_validate(best_svr, X, y, cv=10, scoring=scoring, return_train_score=True)
-    return "MAE :", abs(scores['test_abs_error'].mean()), "| RMSE :", math.sqrt(abs(scores['test_squared_error'].mean()))
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_regression(y_test, y_pred)
 
 
+    elif kernel == 'sigmoid':
+        gsc = GridSearchCV(
+            estimator=SVR(kernel='sigmoid'),
+            param_grid={
+                'C': [0.1, 1, 100, 1000],
+                'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
+                'degree': [2, 3, 4],
+                'coef0': [0.1, 0.01, 0.001, 0.0001],
+                'gamma':['auto']},
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
 
-def svr_model(X, y):
-    gsc = GridSearchCV(
-        estimator=SVR(kernel='rbf'),
-        param_grid={
-            'C': [0.1, 1, 100, 1000],
-            'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
-            'gamma': [0.0001, 0.001, 0.005, 0.1, 1, 3, 5]
-        },
-        cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+        grid_result = gsc.fit(X_train, y_train)
+        best_params = grid_result.best_params_
 
-    grid_result = gsc.fit(X, y)
-    best_params = grid_result.best_params_
-    best_svr = SVR(kernel='rbf', C=best_params["C"], epsilon=best_params["epsilon"], gamma=best_params["gamma"],
-                   coef0=0.1, shrinking=True,
-                   tol=0.001, cache_size=200, verbose=False, max_iter=-1)
+        print('{} best params : {}'.format('sigmoid', best_params))
+        best_svr = SVR(kernel='sigmoid', C=best_params["C"], epsilon=best_params["epsilon"], coef0=best_params["coef0"],                   degree=best_params["degree"], shrinking=True,
+                       tol=0.001, cache_size=200, verbose=False, max_iter=-1, gamma='auto')
 
-    scoring = {
-               'abs_error': 'neg_mean_absolute_error',
-               'squared_error': 'neg_mean_squared_error'}
+        # Train the model using the training sets
+        best_svr.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svr.predict(X_test)
 
-    scores = cross_validate(best_svr, X, y, cv=10, scoring=scoring, return_train_score=True)
-    return "MAE :", abs(scores['test_abs_error'].mean()), "| RMSE :", math.sqrt(abs(scores['test_squared_error'].mean()))
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_regression(y_test, y_pred)
+
+    elif kernel == 'poly':
+        gsc = GridSearchCV(
+            estimator=SVR(kernel='poly'),
+            param_grid={
+                'C': [0.1, 1, 100, 1000],
+                'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
+                'degree': [2, 3, 4],
+                'coef0': [0.1, 0.01, 0.001, 0.0001],
+                'gamma':['auto']},
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+
+        grid_result = gsc.fit(X_train, y_train)
+        best_params = grid_result.best_params_
+
+        print('{} best params : {}'.format('poly', best_params))
+        best_svr = SVR(kernel='poly', C=best_params["C"], epsilon=best_params["epsilon"], coef0=best_params["coef0"],                   degree=best_params["degree"], shrinking=True,
+                       tol=0.001, cache_size=200, verbose=False, max_iter=-1, gamma='auto')
+
+        # Train the model using the training sets
+        best_svr.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svr.predict(X_test)
+
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_regression(y_test, y_pred)
 
 
+    elif kernel == 'rbf':
+
+        gsc = GridSearchCV(
+            estimator=SVR(kernel='rbf'),
+            param_grid={
+                'C': [0.1, 1, 100, 1000],
+                'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10],
+                'gamma': [0.0001, 0.001, 0.005, 0.1, 1, 3, 5]
+            },
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+
+        grid_result = gsc.fit(X, y)
+        best_params = grid_result.best_params_
+        print('{} best params : {}'.format('rbf', best_params))
+        best_svr = SVR(kernel='rbf', C=best_params["C"], epsilon=best_params["epsilon"], gamma=best_params["gamma"],
+                       coef0=0.1, shrinking=True,
+                       tol=0.001, cache_size=200, verbose=False, max_iter=-1)
+
+        # Train the model using the training sets
+        best_svr.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svr.predict(X_test)
+
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_regression(y_test, y_pred)
+
+    elif kernel == 'linear-regression':
+
+        gsc = GridSearchCV(
+            estimator=LinearRegression(normalize=True,),
+            param_grid={
+                'fit_intercept': [True, False]
+            },
+            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
+
+        grid_result = gsc.fit(X, y)
+        best_params = grid_result.best_params_
+        print('{} best params : {}'.format('rbf', best_params))
+        best_svr = LinearRegression(fit_intercept=best_params["fit_intercept"])
+
+        # Train the model using the training sets
+        best_svr.fit(X_train, y_train)
+        # Predict the response for test dataset
+        y_pred = best_svr.predict(X_test)
+
+        # Model Accuracy: how often is the classifier correct?
+        evaluate_for_regression(y_test, y_pred)
 
 
 if __name__ == '__main__':
-    # Run
-    print(svr_model(X, y))
+
+    boston_dataset = load_boston()
+    boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
+    boston['MEDV'] = boston_dataset.target
+    boston.head()
+
+    X = boston.iloc[:, [0, 12]]
+    y = boston.iloc[:, 13]
+
+    # scaler = MinMaxScaler(feature_range=(0, 1))
+    # X = scaler.fit_transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+                                                        random_state=109)  # 70% training and 30% test
 
     # Run
-    print(svr_model(X,y))
+    # print(svr_model(X_train, X_test, y_train, y_test, 'linear'))
     # Run
-    print(svr_model(X, y))
+    # print(svr_model(X_train, X_test, y_train, y_test, 'sigmoid'))
+    # Run
+    # print(svr_model(X_train, X_test, y_train, y_test, 'rbf'))
+    print(svr_model(X_train, X_test, y_train, y_test, 'linear-regression'))
